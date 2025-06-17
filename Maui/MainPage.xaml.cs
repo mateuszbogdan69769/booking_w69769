1d6cdfac0ba4e422d7590d89841ed3ba111ad362
mbogdan <mbogdan.eventzone@gmail.com>
2025-06-17 17:52:41 +0200

    Add MAUI version of APP

---
+++ Maui/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Maui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        CheckConnectionAndLoad();
+    }
+
+    private void CheckConnectionAndLoad()
+    {
+        var current = Connectivity.NetworkAccess;
+
+        if (current == NetworkAccess.Internet)
+        {
+            MyWebView.IsVisible = true;
+            OfflineView.IsVisible = false;
+            MyWebView.Source = "https://app.mbogdan.pl";
+        }
+        else
+        {
+            MyWebView.IsVisible = false;
+            OfflineView.IsVisible = true;
+        }
+
+        Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    }
+
+    private void Connectivity_ConnectivityChanged(object? sender, ConnectivityChangedEventArgs e)
+    {
+        CheckConnectionAndLoad();
+    }
+}
